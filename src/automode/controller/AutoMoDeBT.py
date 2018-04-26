1f4b339b1125b60fd3c6b8e5f3138b81fcd44748
Jonas Kuckling <jonas.kuckling@ulb.ac.be>
2018-04-26 11:34:40 +0200

    Added node types

--- src/automode/controller/AutoMoDeBT.py
+++ src/automode/controller/AutoMoDeBT.py
@@ -1,7 +1,57 @@
 from automode.controller.AutoMoDeControllerABC import AutoMoDeControllerABC
+from abc import ABCMeta, abstractmethod
+from enum import Enum
 
 
 class BT(AutoMoDeControllerABC):
+
+    class ABCNode:
+        __metaclass__ = ABCMeta
+
+        class ReturnCode(Enum):
+            FAIL = -1
+            RUNNING = 0
+            SUCCESS = 1
+
+        def __init__(self):
+            self.children = []
+
+        @abstractmethod
+        def tick(self):
+            pass
+
+        @abstractmethod
+        def draw(self):
+            pass
+
+    class RootNode(ABCNode):
+
+        def tick(self):
+            self.children[0].tick()
+
+        def draw(self):
+            pass
+
+    class SequenceStarNode(ABCNode):
+
+        def tick(self):
+            for child in self.children:
+                return_value = child.tick()
+                if not return_value == BT.ABCNode.ReturnCode.SUCCESS:
+                    # TODO: Last ticked child
+                    return return_value
+            return BT.ABCNode.ReturnCode.SUCCESS
+
+        def draw(self):
+            pass
+
+    class SelectorNode(ABCNode):
+
+        def tick(self):
+            pass
+
+        def draw(self):
+            pass
 
     def __init__(self):
         super().__init__()
