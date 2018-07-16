cb3b2627b578a1f4f9d3f96ba7e70872600631c9
Jonas Kuckling <jonas.kuckling@ulb.ac.be>
2018-07-16 22:52:16 +0200

    Added variable for config file

--- LocalSearch_Submit.sh
+++ LocalSearch_Submit.sh
@@ -9,11 +9,12 @@ TMPDIR=/tmp/$USERNAME/LocalSearch_results
 JOBDIR=/home/$USERNAME/AutoMoDe-LocalSearch
 SOURCEDIR=$JOBDIR/src
 RESULTDIR=$JOBDIR/result
+CONFIG_FILE=config_FSM_agg.ini
 
 mkdir -p $TMPDIR
 source venv/bin/activate &> $TMPDIR/output.txt
 cd $SOURCEDIR
-python3 main.py -c config_FSM.ini -r $TMPDIR &>> $TMPDIR/output.txt
+python3 main.py -c $CONFIG_FILE -r $TMPDIR &>> $TMPDIR/output.txt
 RET=$?
 cd $TMPDIR
 mv * $RESULTDIR
