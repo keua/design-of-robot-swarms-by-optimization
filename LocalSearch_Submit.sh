a85f0cb4f8960b6266c9f81cbfcba0aafca72d94
Jonas Kuckling <jonas.kuckling@ulb.ac.be>
2018-07-17 12:42:13 +0200

    Hopefully better handling in case of failure

--- LocalSearch_Submit.sh
+++ LocalSearch_Submit.sh
@@ -9,15 +9,14 @@ TMPDIR=/tmp/$USERNAME/LocalSearch_results
 JOBDIR=/home/$USERNAME/AutoMoDe-LocalSearch
 SOURCEDIR=$JOBDIR/src
 RESULTDIR=$JOBDIR/result
-CONFIG_FILE=config_FSM_agg.ini
+CONFIG_FILE=config_FSM_for.ini
 
 mkdir -p $TMPDIR
 source venv/bin/activate &> $TMPDIR/output.txt
 cd $SOURCEDIR
 python3 main.py -c $CONFIG_FILE -r $TMPDIR &>> $TMPDIR/output.txt
 RET=$?
-cd $TMPDIR
-mv * $RESULTDIR
+mv $TMPDIR/* $RESULTDIR
 cd $JOBDIR
 rmdir -p $TMPDIR &> /dev/null
 
