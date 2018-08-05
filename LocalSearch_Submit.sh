7e032ac5acd159a7e5bfbb9bb1a44d442abd7d9c
Jonas Kuckling <jonas.kuckling@ulb.ac.be>
2018-08-05 08:37:27 +0200

    Added deprecated warning to LocalSearch_Submit.sh

--- LocalSearch_Submit.sh
+++ LocalSearch_Submit.sh
@@ -4,12 +4,17 @@
 #$ -M jonas.kuckling@ulb.ac.be
 #$ -cwd
 
+# THIS FILE IS DEPRECATED!
+# RATHER USE RunLocalSearch.sh!
+# Only use this file if you are sure that only one process is started.
+# Even then, this script will not be updated anymore and might not behave properly.
+
 USERNAME=`whoami`
 TMPDIR=/tmp/$USERNAME/LocalSearch_results
 JOBDIR=/home/$USERNAME/AutoMoDe-LocalSearch
 SOURCEDIR=$JOBDIR/src
 RESULTDIR=$JOBDIR/result
-CONFIG_FILE=config_FSM_for.ini
+CONFIG_FILE=config_BT_for.ini
 
 mkdir -p $TMPDIR
 source venv/bin/activate &> $TMPDIR/output.txt
