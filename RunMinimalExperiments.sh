181105b2b202b79f71e7a5840539b1188fbfd42c
Jonas Kuckling <jonas.kuckling@ulb.ac.be>
2018-08-30 14:39:38 +0200

    Fixed submit script for minimal case and added script for from_file

--- RunMinimalExperiments.sh
+++ RunMinimalExperiments.sh
@@ -3,8 +3,8 @@
 # run BT_agg
 sh RunLocalSearch.sh BT_agg 10 ./config/minimal
 # run BT_for
-sh RunLocalSearch.sh BT_agg 10 ./config/minimal/
+sh RunLocalSearch.sh BT_for 10 ./config/minimal/
 # run FSM_agg
-sh RunLocalSearch.sh BT_agg 10 ./config/minimal/
+sh RunLocalSearch.sh FSM_agg 10 ./config/minimal/
 # run BT_for
-sh RunLocalSearch.sh BT_agg 10 ./config/minimal/
+sh RunLocalSearch.sh FSM_for 10 ./config/minimal/
