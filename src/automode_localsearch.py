8334e53e06349222fda26ecb855dcc3a608679e6
Jonas Kuckling <jonas.kuckling@ulb.ac.be>
2018-11-08 17:55:51 +0100

    Moved setup logic from localsearch_setup to configuration

--- src/automode_localsearch.py
+++ src/automode_localsearch.py
@@ -6,6 +6,7 @@ Use this script to start the local search. Run start_localsearch.py -h for more 
 
 import argparse
 import logging
+from configuration import BUDGET_DEFAULT, SCENARIO_DEFAULT, RESULT_DEFAULT, JOB_NAME_DEFAULT
 
 
 def load_experiment_file(experiment_file):
@@ -41,17 +42,67 @@ def execute_localsearch():
 
 def parse_arguments():
     """This method parses the arguments to this script"""
+
+    def create_subparser_local():
+        """
+        Sets up the parser for the subcommand local.
+        This subcommand will execute a whole experiment on the local machine.
+        """
+        parser_local = subparsers.add_parser('local', help='run an experiment locally')
+        parser_local.add_argument("-e", dest="experiment_file",
+                                  help="(relative) path to the experiment file")
+
+    def create_subparser_submit():
+        """
+        Sets up the parser for the subcommand submit.
+        This subcommand will submit a whole experiment to the scheduler.
+        """
+        parser_submit = subparsers.add_parser('submit',
+                                              help='submit an experiment to a scheduling system')
+        parser_submit.add_argument("-e", dest="experiment_file",
+                                   help="(relative) path to the experiment file")
+
+    def create_subparser_run():
+        """
+        Sets up the parser for the subcommand run.
+        This subcommand will execute a single local search.
+        """
+        parser_run = subparsers.add_parser('run', help='run a single execution of the local search')
+        # Required arguments
+        parser_run.add_argument('-c', '--config', dest="config_file", required=True,
+                                help="The configuration file for the local search algorithm. "
+                                " (REQUIRED)")
+        parser_run.add_argument('-t', '--controller_type', dest="controller_type", default="FSM", required=True,
+                                help="The type of controller used (FSM or BT). "
+                                "(REQUIRED)")
+        # Recommended arguments
+        parser_run.add_argument('-s', '--scenario_file', dest="scenario_file", default=SCENARIO_DEFAULT, required=False,
+                                help="The scenario file for the improvement. "
+                                "If this is not set, the default from the config file is used.")
+        parser_run.add_argument('-b', '--budget', dest="budget", default=BUDGET_DEFAULT, type=int, required=False,
+                                help="The budget allocated for the localsearch. "
+                                "If this is not set, the default from the config file is used.")
+        parser_run.add_argument('-i', '--initial_controller', dest="initial_controller", default="minimal",
+                                help="The initial controller for the local search. "
+                                "If this is not set, the localsearch will start from a minimal controller.")
+        parser_run.add_argument('-j', '--job_name', dest="job_name", default=JOB_NAME_DEFAULT, required=False,
+                                help="The job name, used for the results folder. "
+                                "If this is not set, one will be created from the scenario, budget and initial "
+                                "controller information.")
+        # Optional arguments
+        parser_run.add_argument('-r', '--result_directory', dest="result_dir", default=RESULT_DEFAULT, required=False,
+                                help="The directory where the results of the local search algorithm are written. "
+                                "If this is not set, the default from the config file is used.")
+        # TODO: Check that -p is at least 2
+        parser_run.add_argument('-p', '--parallel', dest="parallel", default=0, required=False, type=int,
+                                help="The number of parallel nodes reserved for the execution. "
+                                "If this is not set, then the localsearch will be executed completely sequential.")
+
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(title="execution", dest="execution_subcommand")
-    parser_local = subparsers.add_parser('local', help='run an experiment locally')
-    parser_local.add_argument("-e", dest="experiment_file",
-                              help="(relative) path to the experiment file")
-    parser_submit = subparsers.add_parser('submit',
-                                          help='submit an experiment to a scheduling system')
-    parser_submit.add_argument("-e", dest="experiment_file",
-                               help="(relative) path to the experiment file")
-    parser_local = subparsers.add_parser('run', help='run a single execution of the local search')
-    # TODO: Add run configuration (or load it from the helper)
+    create_subparser_local()
+    create_subparser_submit()
+    create_subparser_run()
     input_args = parser.parse_args()
     logging.warning(input_args)
     if input_args.execution_subcommand == "local":
