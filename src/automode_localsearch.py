471cc5b59522bc5a7777df2a04554d4d12e7c360
Jonas Kuckling <jonas.kuckling@ulb.ac.be>
2019-01-08 09:57:04 +0100

    Added exec statement

--- src/automode_localsearch.py
+++ src/automode_localsearch.py
@@ -141,7 +141,7 @@ EOF
 """.format(args["config_file_name"], args["architecture"], args["path_to_scenario"], args["budget"],
            args["initial_controller"], args["result_directory"], job_name=args["job_name"])
     # print(submit_cmd)
-    args = ["qsub", "<<EOF", submit_cmd]
+    args = ["exec", "qsub", "<<EOF", submit_cmd]
     p = subprocess.Popen(args, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     (stdout, stderr) = p.communicate()
     print(stdout.decode('utf-8'))
