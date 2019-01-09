78433bce406c72abe8dc1bae4ef772e4e3c3c960
Jonas Kuckling <jonas.kuckling@ulb.ac.be>
2019-01-09 11:08:58 +0100

    First step of linting automode package

--- src/automode/architecture/__init__.py
+++ src/automode/architecture/__init__.py
@@ -1,9 +1,9 @@
 """
-This module provides access to all implemented control architectures of AutoMoDe.
+This package provides access to abstractions for all implemented control architectures of AutoMoDe
 """
 
 from automode.architecture.AutoMoDeFSM import FSM
 from automode.architecture.AutoMoDeBT import Restricted_BT as BT
-from automode.architecture.AutoMoDeControllerABC import AutoMoDeControllerABC
+from automode.architecture.AutoMoDeArchitectureABC import AutoMoDeArchitectureABC
 
-__all__ = ["AutoMoDeControllerABC", "FSM", "BT"]
+__all__ = ["AutoMoDeArchitectureABC", "FSM", "BT"]
