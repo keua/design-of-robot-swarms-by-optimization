a5bbf2072f56720c8ee917c855a70c69a6cd11c2
kubedaar <kubedaar@ulb.ac.be>
2019-01-29 00:14:29 +0100

    ADD: New version  termination criterion, iteration,discount,time, budget

--- src/localsearch/TerminationCriterion.py
+++ src/localsearch/TerminationCriterion.py
@@ -1,27 +1,75 @@
 
+import math
 import time as tm
+from abc import ABCMeta, abstractmethod
 
 
 class TerminationCriterion(object):
     """
     """
+    __metaclass__ = ABCMeta
 
-    def __init__(self, start=tm.time(), end=15, discount=None, criterion="time"):
+    def __init__(self, start, end):
         """
         """
-        self.criterion = criterion
         self.start = start
         self.end = end
+        self.count = 0
+        self.status = False
+
+    @abstractmethod
+    def satisfied(self):
+        raise NotImplementedError("Subclass must implement abstract method")
+
+
+class TimeTermination(TerminationCriterion):
+    """
+    """
+
+    def satisfied(self):
+        self.count += 1
+        self.status = tm.time() > self.start + self.end * 60
+        return self.status
+
+
+class DiscountTermination(TerminationCriterion):
+    """
+    """
+
+    def __init__(self, start, end, discount):
+        """
+        """
+        super().__init__(start, end)
         self.discount = discount
 
     def satisfied(self):
-        """
-        """
-        if self.criterion == "time":
-            return tm.time() > self.start + self.end * 60
-        elif self.criterion == "discount":
-            self.start *= self.discount
-            return self.start < self.end
-        elif self.criterion == "iteration":
-            self.start += 1
-            return self.start < self.end
+        self.count += 1
+        self.start *= self.discount
+        self.status = self.start < self.end
+        return self.status
+
+
+class IterationTermination(TerminationCriterion):
+    """
+    """
+    @classmethod
+    def from_budget(cls, budget, window_size, window_move):
+        return cls(0, math.floor(budget / (window_size + window_move)))
+
+    def satisfied(self):
+        self.start += 1
+        self.count += 1
+        self.status = self.start >= self.end
+        return self.status
+
+class DualTermination(TerminationCriterion):
+    """
+    """
+    def __init__(self, primary, secondary):
+        self.primary = primary
+        self.secondary = secondary
+
+    def satisfied(self):
+        self.primary.status = self.primary.satisfied()
+        self.secondary.status = self.secondary.satisfied()
+        return self.primary.status or self.secondary.status
