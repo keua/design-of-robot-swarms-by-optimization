3125c6bdfaa6a94c8ec74c5eb058e5efe6cc8de0
kubedaar <kubedaar@ulb.ac.be>
2019-02-10 18:57:39 +0100

    ADD: filename to write

--- src/stats/performance.py
+++ src/stats/performance.py
@@ -9,22 +9,23 @@ def reset():
     best_score = 0
 
 
-def prepare_score_files():
+def prepare_score_files(filename=""):
     if not os.path.isdir("scores"):
         os.mkdir("scores")
-    with open("scores/history.csv", "w") as file:
+    with open("scores/%s_history.csv" % filename, "w") as file:
         file.write("best, perturbed, operator \n")
-    with open("scores/scores.csv", "w") as file:
+    with open("scores/%s_scores.csv" % filename, "w") as file:
         file.write("best_scores; perturbed_scores \n")
 
 
-def save_results(best_controller, perturbed_controller):
-    with open("scores/history.csv", "a") as file:
-        file.write("{}, {}, {} \n".format(best_controller.agg_score[1], perturbed_controller.agg_score[1],
-                                          perturbed_controller.perturb_history[
-                                              len(perturbed_controller.perturb_history) - 1].__name__))
-    with open("scores/scores.csv", "a") as file:
-        file.write("{}; {} \n".format(best_controller.scores, perturbed_controller.scores))
+def save_results(best_controller, perturbed_controller, filename=""):
+    with open("scores/%s_history.csv" % filename, "a") as file:
+        file.write("{}, {}, {} \n".format(
+            best_controller.agg_score[1], perturbed_controller.agg_score[1],
+            perturbed_controller.perturb_history[-1].__name__))
+    with open("scores/%s_scores.csv" % filename, "a") as file:
+        file.write("{}; {} \n".format(
+            best_controller.scores, perturbed_controller.scores))
 
 
 def set_final_score(score):
