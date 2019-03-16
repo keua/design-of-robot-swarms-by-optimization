f79ac5fe63dae9eaaaec3c00f0a690c45e38ddb7
kubedaar <kubedaar@ulb.ac.be>
2019-03-16 15:19:31 +0100

    ADD: New plot to analyze per budget

--- analysis/plot.py
+++ analysis/plot.py
@@ -35,6 +35,10 @@ def initialize():
         help="Type of plot to plot", nargs='+', type=str,
         default=["boxplot"], dest="plots"
     )
+    ap.add_argument(
+        "-t", "--table", required=False, help="print results table",
+        nargs='?', const=bool, type=bool, default=False, dest="table"
+    )
     return ap.parse_args()
 
 def boxplot(args):
@@ -86,10 +90,59 @@ def boxplot(args):
     # Show boxplots
     plt.show()
     # Display Dataframes in Jupyter notebook per budget
-    for i in range(0, rows + 1, 2):
-        display(pandas.concat([dfs[i], dfs[i + 1]], axis=1))
+    if args.table:
+        for i in range(0, rows + 1, 2):
+            display(pandas.concat([dfs[i], dfs[i + 1]], axis=1))
     # Save boxplot for future uses
     fig.savefig("plots/summary_%s.pdf" % args.scenario)
+
+def boxplot_budget(args):
+    """
+    Plot boxplots for budget analysis
+    """
+    df = pandas.read_excel('data/summary_%s.xlsx' % args.scenario, index_col=0)
+    # Generate columns per budget and architecture
+    columns = []
+    for arch in args.architectures:
+        column = []
+        for budget in args.budgets:
+            for acceptance in args.acceptances:
+                column.append("budget_%s_%s_%s" % (budget, arch, acceptance))
+        columns.append(column)
+
+    # Separate data frame in columns per budget and architecture
+    dfs = [df[c] for c in columns]
+
+    # Set plot configuration
+    sns.set_context("notebook")
+    sns.set_style("whitegrid")
+    sns.set_palette("Greys")
+
+    # Define plot structure
+    cols = len(dfs)
+    rows = 1
+    fig, axs = plt.subplots(ncols=cols, nrows=rows, sharey=True, sharex=True)
+
+    # Plot boxplots per each architecture budget and acceptances
+    for i in range(cols):
+        sns.boxplot(data=dfs[i], ax=axs[i])
+    # Set x (budget) label per each architecture
+    for i in range(0, cols):
+        axs[i].set_xticklabels(args.budgets, rotation=30, fontsize=14)
+    # Set titles per each architecture
+    for i, arch in enumerate(args.architectures):
+        axs[i].set_title(arch, fontsize=14)
+    # Set y (objective function) label per each budget
+    for i in range(0, rows):
+        axs[i].set_ylabel('Objective function', fontsize=14)
+    # Show boxplots
+    plt.show()
+    # Display Dataframes in Jupyter notebook per budget
+    if args.table:
+        for i in range(0, len(args.architectures)):
+            display(dfs[i])
+    # Save boxplot for future uses
+    fig.savefig("plots/summary_%s.pdf" % args.scenario)    
 
 def main():
     args = initialize()
